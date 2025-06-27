ca9c51fd214bfdd3810cd84b9ae1a35383a2d58d
Alice <115747654+keth-ket@users.noreply.github.com>
2025-06-27 13:37:53 -0700

    theme toggle adde

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import React from "react";
+
+import { ThemeToggleButton } from "@/components/theme-toggle-button";
 export default function Page() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
-      <Button variant="outline">Click me</Button>
+      <ThemeToggleButton />
     </>
   );
 }
